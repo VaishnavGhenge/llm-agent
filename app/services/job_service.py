6b409d6b6b4ff3d6e7441bba6509d4101ffae68c
Vaishnav Ghenge <vaishnav.ghenge@noovosoft.com>
2024-08-04 03:22:12 +0530

    Add job service apis

--- app/services/job_service.py
+++ app/services/job_service.py
@@ -18,3 +18,14 @@ def create_job_profile(db: Session, job_profile: JobProfileCreate):
 
 def get_job_profile(db: Session, job_id: str):
     return db.query(JobProfileModel).filter(id=job_id).first()
+
+
+def list_job_profile(db: Session):
+    return db.query(JobProfileModel).all()
+
+
+def delete_job_profile(db: Session, job_id: str):
+    db_job_profile = db.query(JobProfileModel).filter(id=job_id).first()
+    db.delete(db_job_profile)
+    db.commit()
+    return db_job_profile
